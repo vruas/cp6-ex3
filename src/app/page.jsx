8e2a65cf9e99f806a367ea47b29edfe9cc1721c1
vruas <vitor-ruas@live.com>
2023-11-06 21:13:46 -0300

    Init Commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1>HOME PAGE</h1>
+
+      
+
+    </>
+  )
+}
---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer className="rodape">
+        <div>
+            <p>Desenvolvido por: <span>pf0670</span></p>
+            <p>NEXT.js - React.js - GitHub - tailwindcss</p>
+        </div>
+    </footer> 
+  )
+}
+ 
